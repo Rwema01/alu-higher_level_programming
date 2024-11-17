060ffc43b12ac278e104fdcc829bf4b1df315f94
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:52:06 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
-""" module uses urllib to make a request """
+"""Fetch https://intranet.hbtn.io/status."""
 import urllib.request
 
 if __name__ == "__main__":
-    """ making request to provided URL """
-    url = 'https://intranet.hbtn.io/status'
-    
-    # Set up a user-agent to mimic a browser request
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-    
-    # Create the Request object with the URL and headers
-    request = urllib.request.Request(url, headers=headers)
-    
-    try:
-        # Open the URL with the request
-        with urllib.request.urlopen(request) as response:
-            html = response.read()
-            print('Body response:')
-            print("\t- type: {}".format(type(html)))
-            print("\t- content: {}".format(html))
-            print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print(f"HTTPError: {e.code} - {e.reason}")
-    except urllib.error.URLError as e:
-        print(f"URLError: {e.reason}")
+    url = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
