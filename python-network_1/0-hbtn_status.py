d43d1d9430c39c3428c57bf13a3bad89eb453f7d
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:35:39 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Fetches and displays the status of a URL.
+"""
+Fetches and displays the status of a URL.
 
 This script fetches the status of a given URL and prints the body of the response in the specified format.
 The script is designed to work for both https://intranet.hbtn.io/status and http://0.0.0.0:5050/status.
 """
 import urllib.request
 
-# URL to fetch
-url = 'https://intranet.hbtn.io/status'  # Change this to 'http://0.0.0.0:5050/status' to use the local server
+url = 'https://intranet.hbtn.io/status'  # Change to 'http://0.0.0.0:5050/status' if needed
 
-# Send the request and get the response
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as response:
     content = response.read()
 
-    # Print the response body
     print("Body response:")
     print("\t- type:", type(content))
     print("\t- content:", content)
