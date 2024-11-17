b56e2588e41035895b28e8ca9977c2da12ed7580
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:56:05 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,15 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(url) as response:
+    url = "https://intranet.hbtn.io/status"
+    
+    # Add a User-Agent header to the request
+    request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
