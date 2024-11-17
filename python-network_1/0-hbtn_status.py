6781297bff2eca4facc5fefba38a9c1938a30163
Rwema01 <r.gisa@alusudent.com>
2024-11-17 04:16:08 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,21 +6,27 @@ using urllib, and prints the body response with the required formatting.
 
 import urllib.request
 
-# Fetching from https://alu-intranet.hbtn.io/status
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-    body = response.read()
+try:
+    # Fetching from https://alu-intranet.hbtn.io/status
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+        body = response.read()
 
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+except Exception as e:
+    print(f"Error fetching https://alu-intranet.hbtn.io/status: {e}")
 
-# Fetching from http://0.0.0.0:5050/status
-with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
-    body = response.read()
+try:
+    # Fetching from http://0.0.0.0:5050/status
+    with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
+        body = response.read()
 
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+except Exception as e:
+    print(f"Error fetching http://0.0.0.0:5050/status: {e}")
 
