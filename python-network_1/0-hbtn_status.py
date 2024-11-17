83a1ea7ad2590c095f7127c52e2def8b1eaf7180
Rwema01 <r.gisa@alusudent.com>
2024-11-17 03:54:49 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-"""
-This script fetches https://alu-intranet.hbtn.io/status using urllib package.
-It displays the response body with proper formatting and decoding.
-The script can be easily updated to fetch data from another URL such as http://0.0.0.0:5050/status (for testing purposes).
-"""
+
 import urllib.request
 
-def fetch_status():
- """
- Fetch the status of the alx intranet site and print formatted body response.
- """
- url = 'https://alu-intranet.hbtn.io/status'
- # Testing purpose
- # url = 'http://0.0.0.0:5050/status'
+with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+ body = response.read()
 
- with urllib.request.urlopen(url) as response:
- data = response.read()
-
- print("Body response:")
- print("- type: {}".format(type(data)))
- print("- content: {}".format(data))
- print("- utf8 content: {}".format(data.decode('utf-8')))
-
-if __name__ == "__main__":
- fetch_status()
+print("Body response:")
+print("- type: {}".format(type(body)))
+print("- content: {}".format(body))
+print("- utf8 content: {}".format(body.decode('utf-8')))
+```
