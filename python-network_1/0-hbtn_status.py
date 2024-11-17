520e9ce8284774566f6bd61f6454084bbefb5eb9
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:50:31 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,13 +7,22 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     
     # Set up a user-agent to mimic a browser request
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
     
-    # Make the request with headers
+    # Create the Request object with the URL and headers
     request = urllib.request.Request(url, headers=headers)
-    with urllib.request.urlopen(request) as response:
-        html = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    
+    try:
+        # Open the URL with the request
+        with urllib.request.urlopen(request) as response:
+            html = response.read()
+            print('Body response:')
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print(f"HTTPError: {e.code} - {e.reason}")
+    except urllib.error.URLError as e:
+        print(f"URLError: {e.reason}")
