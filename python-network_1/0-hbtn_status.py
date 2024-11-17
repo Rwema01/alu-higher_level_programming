9b6b77c80e7687752e2ffad8d3585d691f164489
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:47:34 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" module uses another module to request to make request """
+""" module uses urllib to make a request """
 import urllib.request
 
-
-"""making request to provided url"""
 if __name__ == "__main__":
-    """making request to provided url"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    """ making request to provided URL """
+    url = 'https://intranet.hbtn.io/status'
+    
+    # Set up a user-agent to mimic a browser request
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    
+    # Make the request with headers
+    request = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(request) as response:
         html = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        """ the method i used is easy way for string manipulation 
-            to use a method that make use of the concept
-            print out html.decode('utf-8') with string formatting
-        """
-        temp = str(html)
-        print("\t- utf8 content: {}".format(temp[2:-1]))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
