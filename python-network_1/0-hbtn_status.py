1f7937686f17a0eeef32f18924401fe90eaf19cf
Rwema01 <r.gisa@alusudent.com>
2024-11-17 04:06:17 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+"""
+This script fetches https://alu-intranet.hbtn.io/status and http://0.0.0.0:5050/status
+using urllib, and prints the body response with the required formatting.
+"""
 
 import urllib.request
 
 with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
- body = response.read()
+    body = response.read()
 
- print("Body response:")
- print(" - type: {}".format(type(body)))
- print(" - content: {}".format(body))
- print(" - utf8 content: {}".format(body.decode('utf-8')))
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode('utf-8')))
 
 with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
- body = response.read()
+    body = response.read()
 
- print("Body response:")
- print(" - type: {}".format(type(body)))
- print(" - content: {}".format(body))
- print(" - utf8 content: {}".format(body.decode('utf-8')))
+    print("Body response:")
+    print("    - type: {}".format(type(body)))
+    print("    - content: {}".format(body))
+    print("    - utf8 content: {}".format(body.decode('utf-8')))
