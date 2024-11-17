076519a6e8d64b238cfd11256a1f43e9c1d48073
Rwema01 <r.gisa@alusudent.com>
2024-11-17 01:43:32 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-"""Fetches the status of a URL and displays the response body.
-
-This script fetches the status of a given URL and prints the body of the response
-in the specified format. It works with the URL 'https://intranet.hbtn.io/status'.
-"""
+""" module uses another module to request to make request """
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
 
-# Updated User-Agent header without unnecessary new lines
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-}
-
-# Request the URL with the custom headers
-req = urllib.request.Request(url, headers=headers)
-
-# Open the URL and read the response
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-
-    # Print the response details
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
-
+"""making request to provided url"""
+if __name__ == "__main__":
+    """making request to provided url"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        """ the method i used is easy way for string manipulation 
+            to use a method that make use of the concept
+            print out html.decode('utf-8') with string formatting
+        """
+        temp = str(html)
+        print("\t- utf8 content: {}".format(temp[2:-1]))
