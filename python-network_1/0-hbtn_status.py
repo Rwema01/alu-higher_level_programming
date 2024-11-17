2b53c501fe79f62c577fbc0f9743508be00a6285
Rwema01 <r.gisa@alusudent.com>
2024-11-17 03:59:51 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,14 @@ with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
  body = response.read()
 
 print("Body response:")
-print("- type: {}".format(type(body)))
-print("- content: {}".format(body))
-print("- utf8 content: {}".format(body.decode('utf-8')))
-```
+print(" - type: {}".format(type(body)))
+print(" - content: {}".format(body))
+print(" - utf8 content: {}".format(body.decode('utf-8')))
+
+with urllib.request.urlopen('http://0.0.0.0:5050/status') as response:
+ body = response.read()
+
+print("Body response:")
+print(" - type: {}".format(type(body)))
+print(" - content: {}".format(body))
+print(" - utf8 content: {}".format(body.decode('utf-8')))
