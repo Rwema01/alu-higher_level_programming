403006569c2700b6a9a65a06a8609eee2805485b
Rwema01 <r.gisa@alusudent.com>
2024-11-17 03:45:52 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """
-This script fetches https://alu-intranet.hbtn.io/status using urllib package.
-It displays the response body with proper formatting and decoding.
-The script can be easily updated to fetch data from another URL such as http://0.0.0.0:5050/status (for testing purposes).
-"""
+Fetch status of alx.intranet site
+ This script fetches https://alu-intranet.hbtn.io/status using urllib package.
+ It displays the response body with proper formatting and decoding.
+ The script can be easily updated to fetch data from another URL such as http://0.0.0.0:5050/status (for testing purposes).
 
+"""
 import urllib.request
 
-def fetch_status_main():
+def fetch_status():
  """
- Fetch the status of the URL and print formatted body response.
- The URL used is https://alu-intranet.hbtn.io/status, but it can be updated for testing.
+ Fetch the status of the alx intranet site and print formatted body response.
  """
- url = 'https://alu-intranet.hbtn.io/status' # Default URL for production
+ url = 'https://alu-intranet.hbtn.io/status'
  # Testing purpose
  # url = 'http://0.0.0.0:5050/status'
 
  with urllib.request.urlopen(url) as response:
- body = response.read()
+ data = response.read()
 
  print("Body response:")
- print(f"\t- type: {type(body)}")
- print(f"\t- content: {body}")
- print(f"\t- utf8 content: {body.decode('utf-8')}")
+ print("\t- type: ", type(data))
+ print("\t- content: ", data)
+ print("\t- utf8 content: ", data.decode('utf-8'))
 
 if __name__ == "__main__":
- fetch_status_main()
+ fetch_status()
