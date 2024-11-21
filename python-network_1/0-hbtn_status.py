7e92de5c9dad7dd3e9c15983ee5b5a7c58df13f8
Rwema01 <r.gisa@alusudent.com>
2024-11-21 20:51:52 +0000

    get

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-"""
-This script fetches the status of two different URLs
-and displays the response content in various formats.
-
-It fetches:
-    - https://alu-intranet.hbtn.io/status
-    - http://0.0.0.0:5050/status (for local server)
-
-The script shows the type, content, and UTF-8 decoded content of the response.
+"""A script that
+fetches https://intranet.hbtn.io/status.
 """
 
-import urllib.request
 
-def fetch_status(url):
-    """Fetches and prints the status of a given URL"""
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
         print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}")
-
-# Fetch status for the first URL
-url_1 = "https://alu-intranet.hbtn.io/status"
-fetch_status(url_1)
-
-# Fetch status for the second URL
-url_2 = "http://0.0.0.0:5050/status"
-fetch_status(url_2)
-
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
