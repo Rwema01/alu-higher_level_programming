29a86749b656d625de9672fc381457375692cbfd
Rwema01 <r.gisa@alustudent.com>
2024-11-28 20:51:52 +0200

    do

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Changes the name of the State object where id=2 to New Mexico from a database
+Deletes all State objects with a name containing the letter 'a' from a database
 """
 
 import sqlalchemy
@@ -9,16 +9,26 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
+    # Connect to the MySQL database using the provided credentials
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    # Create the tables if they don't already exist
     Base.metadata.create_all(eng)
+
+    # Start a session to interact with the database
     Session = sessionmaker(bind=eng)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a%'))
+
+    # Query to find all states where the name contains the letter 'a' (case insensitive)
+    states = session.query(State).filter(State.name.ilike('%a%')).all()  # Using ilike for case-insensitive search
+
+    # Loop through the states and delete them
     for state in states:
         session.delete(state)
+
+    # Commit the changes to the database
     session.commit()
+
+    # Close the session
     session.close()
