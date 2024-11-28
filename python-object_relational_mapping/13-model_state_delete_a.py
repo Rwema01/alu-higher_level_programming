bce3e8a02192ed95a244f2ea2b55773a9081395c
Rwema01 <r.gisa@alustudent.com>
2024-11-28 21:04:26 +0200

    do

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,11 +5,14 @@ from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
@@ -20,5 +23,4 @@ if __name__ == "__main__":
         session.delete(row)
 
     session.commit()
-
     session.close()
